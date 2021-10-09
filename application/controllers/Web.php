2d7e1e3da8fca57f67fe69a46614f5e5e02d72e7
shelyfebriyanty <sellyfebryanty@gmail.com>
2021-10-09 09:23:18 -0700

    tugas3
    Signed-off-by: shelyfebriyanty <sellyfebryanty@gmail.com>

---
+++ application/controllers/Web.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') or exit('no direct script access allowed');
+
+class web extends CI_Controller
+{
+    
+    function _construct()
+    {
+        parent::_construct();
+        $this->load->helper('url');
+    }
+
+    public function index()
+    {
+        $data['judul'] = "Halaman Depan";
+        $this->load->view('v_header', $data);
+        $this->load->view('v_index', $data);
+        $this->load->view('v_footer', $data);
+    }
+
+    public function about()
+    {
+        $data['judul'] = "Halaman About";
+        $this->load->view('v_header', $data);
+        $this->load->view('v_about', $data);
+        $this->load->view('v_footer', $data);
+    }
+
+
+
+
+
+
+
---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Shely</li>
+        <li> Nama Belakang : Febriyanty</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Kp. Kebun Jeruk No. 07 Kabupaten Bogor</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+    </ul>
+</section>
+
