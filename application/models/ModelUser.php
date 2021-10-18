073371f9391677347d3aa37b3440fedab36f7361
shelyfebriyanty <sellyfebryanty@gmail.com>
2021-10-18 10:10:30 -0700

    tugas5
    Signed-off-by: shelyfebriyanty <sellyfebryanty@gmail.com>

---
+++ application/models/ModelUser.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Modeluser extends CI_Model
+{
+    public function simpanData($data = null)
+    {
+        $this->db->insert('user', $data);
+    }
+    
+    public function cekData($where = null)
+    {
+        return $this->db->get_where('user', $where);
+    }
+
+    public function getUserwhere($where = null)
+    {
+        return $this->db>get_where('user', $where);
+    }
+
+    public function cekUserAccess($where = null)
+    {
+        $this->db->select('*');
+        $this->db->from('access_menu');
+        $this->db->where($where);
+        return $this->db->get();
+    }
+
+    public function getuserlimit()
+    {
+        $this->db->select('*');
+        $this->db->from('user');
+        $this->db->limit(10, 0);
+        return $this->db->get();
+    }
